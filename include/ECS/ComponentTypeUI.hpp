4af05d1cdd689123aa0489a7d44b94b982314742
ClairDeLuna01 <luna.bossu@gmail.com>
2025-10-03 19:52:54 +0200

    renamed macro to full caps snake case for clarity

--- include/ECS/ComponentTypeUI.hpp
+++ include/ECS/ComponentTypeUI.hpp
@@ -1,12 +1,13 @@
 #pragma once 
 
+#include "MappedEnum.hpp"
 #include <Graphics/ObjectGroup.hpp>
 #include <Graphics/FastUI.hpp>
 #include <functional>
 
 #define UNINITIALIZED_FLOAT 1e12f
 
-#define ChainedMember(classt, type, name, defval) \
+#define CHAINED_MEMBER(classt, type, name, defval) \
     type name = defval; \
     classt & set##name(const type & new_##name = defval){name = new_##name; return *this;};
 
@@ -51,6 +52,7 @@ struct WidgetBox
     {
         FOLLOW_PARENT_BOX, FOLLOW_SIBLINGS_BOX
     } type;
+
 
     WidgetBox(
         vec2 xrange = vec2(-1, 1), 
@@ -161,12 +163,12 @@ struct WidgetButton
         UpdateFunc2D valueUpdate2D
         ) : type(type), valueChanged2D(valueChanged2D), valueUpdate2D(valueUpdate2D){};
 
-    ChainedMember(WidgetButton, float, cur, 1)
-    ChainedMember(WidgetButton, float, cur2, 0)
-    ChainedMember(WidgetButton, float, min, 0)
-    ChainedMember(WidgetButton, float, max, 1)
-    ChainedMember(WidgetButton, float, padding, 1e3f)
-    ChainedMember(WidgetButton, uint64, usr, 0)
+    CHAINED_MEMBER(WidgetButton, float, cur, 1)
+    CHAINED_MEMBER(WidgetButton, float, cur2, 0)
+    CHAINED_MEMBER(WidgetButton, float, min, 0)
+    CHAINED_MEMBER(WidgetButton, float, max, 1)
+    CHAINED_MEMBER(WidgetButton, float, padding, 1e3f)
+    CHAINED_MEMBER(WidgetButton, uint64, usr, 0)
 
     InteractFunc valueChanged;
     UpdateFunc valueUpdate;
@@ -179,17 +181,17 @@ struct WidgetButton
 
 struct WidgetStyle
 {
-    ChainedMember(WidgetStyle, vec4, textColor1, vec4(vec3(0.85), 1))
-    ChainedMember(WidgetStyle, vec4, textColor2, vec4(0.85, 0.85, 0.25, 1))
-    ChainedMember(WidgetStyle, vec4, backgroundColor1, vec4(vec3(0.35), 0.9))
-    ChainedMember(WidgetStyle, vec4, backgroundColor2, vec4(vec3(0.125), 0.9))
-    ChainedMember(WidgetStyle, UiTileType, backGroundStyle, UiTileType::SQUARE)
-    ChainedMember(WidgetStyle, int, automaticTabbing, 0)
-    ChainedMember(WidgetStyle, float, spriteScale, 1)
+    CHAINED_MEMBER(WidgetStyle, vec4, textColor1, vec4(vec3(0.85), 1))
+    CHAINED_MEMBER(WidgetStyle, vec4, textColor2, vec4(0.85, 0.85, 0.25, 1))
+    CHAINED_MEMBER(WidgetStyle, vec4, backgroundColor1, vec4(vec3(0.35), 0.9))
+    CHAINED_MEMBER(WidgetStyle, vec4, backgroundColor2, vec4(vec3(0.125), 0.9))
+    CHAINED_MEMBER(WidgetStyle, UiTileType, backGroundStyle, UiTileType::SQUARE)
+    CHAINED_MEMBER(WidgetStyle, int, automaticTabbing, 0)
+    CHAINED_MEMBER(WidgetStyle, float, spriteScale, 1)
 
-    ChainedMember(WidgetStyle, bool, useInternalSpacing, false)
+    CHAINED_MEMBER(WidgetStyle, bool, useInternalSpacing, false)
 
-    ChainedMember(WidgetStyle, vec2, spritePosition, vec2(0))
+    CHAINED_MEMBER(WidgetStyle, vec2, spritePosition, vec2(0))
 
     bool useAltBackgroundColor = false;
     bool useAltTextColor = false;
